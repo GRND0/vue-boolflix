cc509f6c2aba97f8351aba0a4a4a3c0597d81ea7
GRND0 <zefiro.turchetta@gmail.com>
2022-05-23 19:22:55 +0200

    bozza milestone 2

--- src/components/FlixMain.vue
+++ src/components/FlixMain.vue
@@ -1,18 +1,23 @@
 <template>
     <div>
         <div class="card" v-for="(items, index) in items" :key="index">
-            <h2> {{ items.title }}</h2>
-            <h2>{{ items.original_title }}</h2>
-            <h2>{{items.original_language}}</h2>
-            <h2>{{items.vote_average}}</h2>
+            <h4> {{ items.title }}</h4>
+            <h4>{{ items.original_title }}</h4>
+            <country-flag country="item.original_language" />
+            <h4>{{items.vote_average}}</h4>
         </div>
 
     </div>
 </template>
 
 <script>
+import CountryFlag from 'vue-country-flag';
+
 export default {
     name: "FlixMain",
+    components : {
+        CountryFlag,
+    },
     props: {
         items: Array,
     },
