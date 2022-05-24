99a74e52040734d7855c9a055de89db703b0d54c
GRND0 <zefiro.turchetta@gmail.com>
2022-05-24 17:22:40 +0200

    bozza milestone 4

--- src/components/FlixHeader.vue
+++ src/components/FlixHeader.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-        <input type="text" placeholder="Cerca film o serie TV" v-model="ricerca">
-        <button @click="$emit('search', ricerca)">Cerca</button>
+    <div class="header"> 
+        <div class="logo"><img src="../assets/c072857f28ffed69c9266a55183f4c20.png" alt=""></div>
+        <div class="wrapper-ricerca">
+            <input type="text" placeholder="Cerca film o serie TV" v-model="ricerca">
+            <button @click="$emit('search', ricerca)">Cerca</button>
+        </div>
     </div>
 </template>
 
@@ -21,4 +24,42 @@ export default {
 
 <style lang="scss">
 @import "../style/common.scss";
+
+.header {
+    height: 50px;
+    width: 100%;
+    display: flex;
+    background-color: black;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 2px solid gray;
+    
+    .logo {
+        height: 70%;
+        margin-left: 2rem;
+
+        img {
+            height: 100%;
+        }
+    }
+
+    .wrapper-ricerca {
+        margin-right: 2rem;
+
+        button {
+            background-color: red;
+            color: white;
+            border-radius: 5px;
+            border: none;
+            padding: 0.2rem;
+        }
+
+        input {
+            margin: 1rem;
+            border-radius: 5px;
+            border: none;
+            padding: 0.2rem;
+        }
+    }
+}
 </style>
--- src/components/FlixMain.vue
+++ src/components/FlixMain.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
-        <div class="card" v-for="(item, index) in items" :key="index">
-            <h4> {{ item.title }}</h4>
-            <h4>{{ item.name }}</h4>
-            <h4>{{ item.original_name }}</h4>
-            <h4>{{ item.original_title }}</h4>
-            <country-flag :country="item.original_language" />
-            <h4>{{ divisioneArrotondata(item.vote_average) }}</h4>
-            <!-- <star-rating v-model="voto" read-only="true"></star-rating> -->
-            <span v-for="number in divisioneArrotondata(item.vote_average) " :key="number"> <i class="fas fa-star"> </i></span>
-            <img :src="`http://image.tmdb.org/t/p/w500/${item.poster_path}`" alt="">
+        <div class="wrapper">
+            <div class="card" v-for="(item, index) in items" :key="index">
+                <div class="wrapper-info" :class="{invisibile: nascosto}"  >
+                    <h4> Titolo : {{ item.title }}</h4>
+                    <h4> TItolo : {{ item.name }}</h4>
+                    <h4> Titolo originale : {{ item.original_name }}</h4>
+                    <h4> TItolo originale : {{ item.original_title }}</h4>
+                    <country-flag :country="item.original_language" />
+                    <h4> Voto {{ divisioneArrotondata(item.vote_average) }}</h4>
+                    <!-- <star-rating v-model="voto" read-only="true"></star-rating> -->
+                    <span v-for="number in divisioneArrotondata(item.vote_average) " :key="number"> <i
+                            class="fas fa-star">
+                        </i></span>
+                </div>
+                <img class="wrapper-flip" :src="`http://image.tmdb.org/t/p/w500/${item.poster_path}`" alt="">
+            </div>
         </div>
 
     </div>
@@ -31,7 +37,8 @@ export default {
     },
     data: function () {
         return {
-        
+            nascosto : true 
+
         }
     },
     methods: {
@@ -50,4 +57,39 @@ export default {
 <style lang="scss">
 @import '~@fortawesome/fontawesome-free/css/all.min.css';
 @import "../style/common.scss";
+
+.card .wrapper-info {
+    display: none;
+}
+
+.card:hover .wrapper-info {
+    display: block;
+}
+
+.card:hover .wrapper-flip {
+    display: none;
+}
+
+.invisibile {
+    display: none;
+}
+
+.wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    height: calc(100vh - 50px);
+    padding: 1rem;
+    background-color: black;
+    overflow-y: scroll;
+
+    .card {
+        width: calc(100% / 6 - 2rem);
+        margin: 0.5rem;
+        background-color: darkgrey;
+        border: solid 1px white;
+
+    }
+
+}
 </style>
